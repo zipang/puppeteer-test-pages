47c991f16c8d090fe907f1870e7e766803c15bf2
zipang <christophe.Desguez@gmail.com>
2023-11-20 17:22:51 +0100

    Explain the different scenarios in the pages index

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { Center } from "../layout/Center.jsx";
 import { MainLayout } from "../layout/MainLayout.jsx";
 
@@ -5,15 +6,32 @@ function MainMenu() {
 	return (
 		<MainLayout>
 			<Center>
-				<div>
+				<section id="main-menu">
 					<h1>Hello Puppeteer.</h1>
-					<h2>Here are a few navigation links to our test pages</h2>
-					<ul>
+					<p>
+						Here are a few navigation links to our test pages implementing
+						different tests scenarios
+					</p>
+					<ul className="bg-blue-50 py-4 px-8 list-disc">
 						<li>
-							<a href="/waitfor">Wait for className</a>
+							<Link
+								className="underline text-sky-400 hover:bg-teal-100"
+								to="/waitfor"
+							>
+								Wait for the Blue Box (will appear after 5secs)
+							</Link>
+						</li>
+						<li>
+							<Link
+								className="underline text-sky-400 hover:bg-teal-100"
+								to="/waitfor?fail=true"
+							>
+								Wait for the Blue Box (will not appear and throw after
+								5secs)
+							</Link>
 						</li>
 					</ul>
-				</div>
+				</section>
 			</Center>
 		</MainLayout>
 	);
